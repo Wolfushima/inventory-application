e1e0827980f26a0d7152df34f552f2eeb8f2114c
Adolfo Herrera <valherdolf@gmail.com>
2023-09-15 20:43:47 -0400

    Create 'CameraCategory' model

---
+++ models/camera-category.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const CameraCategorySchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        enum: ['Digital', 'Film'],
+    },
+    description: { type: String, required: true },
+});
+
+// Virtual for camera-category's URL
+CameraCategorySchema.virtual('url').get(function () {
+    return `/catalog/cameracategory/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model('CameraCategory', CameraCategorySchema);

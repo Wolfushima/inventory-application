92cf9b22950fd51650c3204c5c8a04fd3fd9db21
Adolfo Herrera <valherdolf@gmail.com>
2023-09-26 17:30:31 -0400

    Add virtual to return formatted dates

--- models/camera.js
+++ models/camera.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const { Schema } = mongoose;
 
@@ -31,6 +32,21 @@ const CameraSchema = new Schema({
 // Virtual for camera's URL
 CameraSchema.virtual('url').get(function () {
     return `/catalog/camera/${this._id}`;
+});
+
+// Virtual for camera's date of release formatted
+CameraSchema.virtual('date_of_release_formatted').get(function () {
+    return DateTime.fromJSDate(this.date_of_release).toLocaleString(
+        DateTime.DATE_MED,
+    );
+});
+
+// Virtual for camera's date of discontinuation formatted
+CameraSchema.virtual('date_of_discontinuation_formatted').get(function () {
+    if (!this.date_of_discontinuation) return 'N/A';
+    return DateTime.fromJSDate(this.date_of_discontinuation).toLocaleString(
+        DateTime.DATE_MED,
+    );
 });
 
 // Export model

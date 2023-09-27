7050d32fd2c7e2cddd47cd943d5b8f12b6ce8cb5
Adolfo Herrera <valherdolf@gmail.com>
2023-09-27 19:58:07 -0400

    Add virtual to return formatted dates

--- models/brand.js
+++ models/brand.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const { Schema } = mongoose;
 
@@ -15,6 +16,21 @@ const BrandSchema = new Schema({
 // Virtual for brand's URL
 BrandSchema.virtual('url').get(function () {
     return `/catalog/brand/${this._id}`;
+});
+
+// Virtual for brand's date of founding formatted
+BrandSchema.virtual('date_of_founding_formatted').get(function () {
+    return DateTime.fromJSDate(this.date_of_founding).toLocaleString(
+        DateTime.DATE_MED,
+    );
+});
+
+// Virtual for brand's date of defunct formatted
+BrandSchema.virtual('date_of_defunct_formatted').get(function () {
+    if (!this.date_of_defunct) return 'N/A';
+    return DateTime.fromJSDate(this.date_of_defunct).toLocaleString(
+        DateTime.DATE_MED,
+    );
 });
 
 // Export model

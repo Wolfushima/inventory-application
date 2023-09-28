1d78b82356bc5289d0de778fc4ba4005ef93c92f
Adolfo Herrera <valherdolf@gmail.com>
2023-09-28 19:58:01 -0400

    Implement function to display camera category create form on GET and POST

--- controllers/camera-category-controller.js
+++ controllers/camera-category-controller.js
@@ -1,4 +1,5 @@
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
 const CameraCategory = require('../models/camera-category');
 const Camera = require('../models/camera');
 
@@ -41,14 +42,58 @@ exports.cameracategory_detail = asyncHandler(async (req, res, next) => {
 });
 
 // Display CameraCategory create form on GET.
-exports.cameracategory_create_get = asyncHandler(async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: CameraCategory create GET');
-});
+exports.cameracategory_create_get = (req, res, next) => {
+    res.render('cameracategory_form', { title: 'Create Camera Category' });
+};
 
 // Handle CameraCategory create on POST.
-exports.cameracategory_create_post = asyncHandler(async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: CameraCategory create POST');
-});
+exports.cameracategory_create_post = [
+    // Validate and sanitize the name field.
+    body('name', 'Camera Category must contain at least 3 characters.')
+        .trim()
+        .isLength({ min: 3 })
+        .escape(),
+    body('description', 'Description must not be empty.')
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+
+    // Process request after validation and sanitization.
+    asyncHandler(async (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create a cameracategory object with escaped and trimmed data.
+        const cameraCategory = new CameraCategory({
+            name: req.body.name,
+            description: req.body.description,
+        });
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render the form again with sanitized values/error messages.
+            res.render('cameracategory_form', {
+                title: 'Create Camera Category',
+                camera_category: cameraCategory,
+                errors: errors.array(),
+            });
+            return;
+        }
+
+        // Data from form is valid.
+        // Check if CameraCategory with same name already exists.
+        const cameraCategoryExists = await CameraCategory.findOne({
+            name: req.body.name,
+        }).exec();
+        if (cameraCategoryExists) {
+            // CameraCategory exists, redirect to its detail page.
+            res.redirect(cameraCategoryExists.url);
+        } else {
+            await cameraCategory.save();
+            // New cameracategory saved. redirect to cameracategory detail page.
+            res.redirect(cameraCategory.url);
+        }
+    }),
+];
 
 // Display CameraCategory delete form on GET.
 exports.cameracategory_delete_get = asyncHandler(async (req, res, next) => {

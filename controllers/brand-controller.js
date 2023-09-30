ee312bee440f188605afecd5c18c2370d7dd9073
Adolfo Herrera <valherdolf@gmail.com>
2023-09-29 21:31:30 -0400

    Implement function to display brand create form on GET and handle brand create form on POST

--- controllers/brand-controller.js
+++ controllers/brand-controller.js
@@ -1,4 +1,5 @@
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
 const Brand = require('../models/brand');
 const Camera = require('../models/camera');
 
@@ -35,13 +36,75 @@ exports.brand_detail = asyncHandler(async (req, res, next) => {
 
 // Display Brand create form on GET.
 exports.brand_create_get = asyncHandler(async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Brand create GET');
+    res.render('brand_form', { title: 'Create Brand' });
 });
 
 // Handle Brand create on POST.
-exports.brand_create_post = asyncHandler(async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Brand create POST');
-});
+exports.brand_create_post = [
+    // Convert founder to an array.
+    (req, res, next) => {
+        if (!(req.body.founder instanceof Array)) {
+            if (typeof req.body.founder === 'undefined') req.body.founder = [];
+            else req.body.founder = new Array(req.body.founder);
+        }
+        next();
+    },
+
+    // Validate and sanitize fields.
+    body('name', 'Name of brand must be specified.')
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body('description', 'Description must not be empty.')
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body('date_of_founding', 'Invalid date of founding.')
+        .optional({ values: 'falsy' })
+        .isISO8601()
+        .toDate(),
+    body('date_of_defunct', 'Invalid date of defunct.')
+        .optional({ values: 'falsy' })
+        .isISO8601()
+        .toDate(),
+    body('founder.*', 'Founder must not be empty.')
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body('headquarted').trim().escape(),
+    body('website').trim().escape(),
+
+    // Process request after validation and sanitization.
+    asyncHandler(async (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create Brand object with escaped and trimmed data
+        const brand = new Brand({
+            name: req.body.name,
+            description: req.body.description,
+            date_of_founding: req.body.date_of_founding,
+            date_of_defunct: req.body.date_of_defunct,
+            founder: req.body.founder,
+            headquarters: req.body.headquarters,
+            website: req.body.website,
+        });
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/errors messages.
+            res.render('brand_form', {
+                title: 'Create Brand',
+                brand,
+                errors: errors.array(),
+            });
+            return;
+        }
+
+        // Data from form is valid. Save brand.
+        await brand.save();
+        res.redirect(brand.url);
+    }),
+];
 
 // Display Brand delete form on GET.
 exports.brand_delete_get = asyncHandler(async (req, res, next) => {

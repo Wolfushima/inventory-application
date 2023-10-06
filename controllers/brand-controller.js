8d4891e533611cb35f590309dd3679e8c93755ea
Adolfo Herrera <valherdolf@gmail.com>
2023-10-05 21:23:00 -0400

    Implement function to update a brand

--- controllers/brand-controller.js
+++ controllers/brand-controller.js
@@ -149,10 +149,84 @@ exports.brand_delete_post = asyncHandler(async (req, res, next) => {
 
 // Display Brand update form on GET.
 exports.brand_update_get = asyncHandler(async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Brand update GET');
+    const brand = await Brand.findById(req.params.id).exec();
+    if (brand === null) {
+        // No results.
+        const err = new Error('Brand not found.');
+        err.status = 404;
+        return next(err);
+    }
+
+    res.render('brand_form', {
+        title: 'Update Brand',
+        brand,
+    });
 });
 
 // Handle Brand update on POST.
-exports.brand_update_post = asyncHandler(async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Brand update POST');
-});
+exports.brand_update_post = [
+    // Convert founder to an array.
+    (req, res, next) => {
+        if (!(req.body.founder instanceof Array)) {
+            if (typeof req.body.founder === 'undefined') req.body.founder = [];
+            else req.body.founder = new Array(req.body.founder);
+        }
+        next();
+    },
+
+    // Validate and sanitize fields.
+    body('name', 'Name of brand must be specified.')
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body('description', 'Description must not be empty.')
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body('date_of_founding', 'Invalid date of founding.')
+        .optional({ values: 'falsy' })
+        .isISO8601()
+        .toDate(),
+    body('date_of_defunct', 'Invalid date of defunct.')
+        .optional({ values: 'falsy' })
+        .isISO8601()
+        .toDate(),
+    body('founder.*', 'Founder must not be empty.')
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body('headquarted').trim().escape(),
+    body('website').trim().escape(),
+
+    // Process request after validation and sanitization.
+    asyncHandler(async (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create Brand object with escaped and trimmed data
+        const brand = new Brand({
+            name: req.body.name,
+            description: req.body.description,
+            date_of_founding: req.body.date_of_founding,
+            date_of_defunct: req.body.date_of_defunct,
+            founder: req.body.founder,
+            headquarters: req.body.headquarters,
+            website: req.body.website,
+            _id: req.params.id,
+        });
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/errors messages.
+            res.render('brand_form', {
+                title: 'Update Brand',
+                brand,
+                errors: errors.array(),
+            });
+            return;
+        }
+
+        // Data from form is valid. Save brand.
+        await Brand.findByIdAndUpdate(req.params.id, brand);
+        res.redirect(brand.url);
+    }),
+];
